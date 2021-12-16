96cb42a9202b257237ae1e2449236c7312d38736
Sbuchelov <mikivolkoff@ya.ru>
2021-12-16 16:54:33 +0300

    валидация для корзины

--- src/main/java/ru/gb/gbrest/dto/CartDto.java
+++ src/main/java/ru/gb/gbrest/dto/CartDto.java
@@ -2,6 +2,8 @@ package ru.gb.gbrest.dto;
 
 import lombok.*;
 import ru.gb.gbrest.entity.Product;
+
+import javax.validation.constraints.NotNull;
 import java.util.Set;
 
 @Getter
@@ -11,6 +13,8 @@ import java.util.Set;
 @Builder
 public class CartDto {
     private Long id;
+    @NotNull
     private String status;
+    @NotNull
     private Set<Product> products;
 }
